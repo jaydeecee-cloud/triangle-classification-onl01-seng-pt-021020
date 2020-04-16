fb4a1013d7faa82d56fc469fe4c828fc28b5ec78
Joseph Choi <jdc07@live.com>
2020-04-16 18:33:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,11 +15,13 @@ class Triangle
     else
       :scalene
     end
+  end
 
   def validate_triangle
     real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
     [a, b, c].each { |s| real_triangle << false if s <= 0 }
     raise TriangleError if real_triangle.include?(false)
+  end
   end
 
   class TriangleError < StandardError
